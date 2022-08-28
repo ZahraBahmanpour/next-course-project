2d30934cd815a09d25ae7255d45c7b5b0b606a9f
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-28 12:58:12 +0430

    add Layout to _app

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -8,10 +8,10 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
-            <Link to="/">All Meetups</Link>
+            <Link href="/">All Meetups</Link>
           </li>
           <li>
-            <Link to="/new-meetup">Add New Meetup</Link>
+            <Link href="/new-meetup">Add New Meetup</Link>
           </li>
         </ul>
       </nav>
@@ -20,4 +20,3 @@ function MainNavigation() {
 }
 
 export default MainNavigation;
-Footer;
