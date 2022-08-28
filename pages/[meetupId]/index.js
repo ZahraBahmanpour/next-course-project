eb2a68ae321c717c7967076f6d77de12d9d6c8dc
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-28 14:47:23 +0430

    add MeetupDetail coponent

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      title={"First Meetup"}
+      description={"This is First Meetup Description"}
+    />
+  );
+};
+
+export default MeetupDetails;
